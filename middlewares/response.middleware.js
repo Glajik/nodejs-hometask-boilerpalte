7511f8f75be3851e9b1bea8648ddef07def007b9
Viacheslav Glazhevskyi <glajik@gmail.com>
2020-05-16 23:28:53 +0300

    Implements responseMiddleware and use for error handling

--- middlewares/response.middleware.js
+++ middlewares/response.middleware.js
@@ -1,5 +1,26 @@
-const responseMiddleware = (req, res, next) => {
-   // TODO: Implement middleware that returns result of the query
+// Middleware that returns result of the query
+const responseMiddleware = (err, req, res, next) => {
+    switch (err.message) {
+        case 'USER_NOT_FOUND':
+            res.status(404).json({
+                error: true,
+                message: 'User not found',
+            });
+            break;
+            
+        case 'USER_CREATE_ERROR':
+            res.status(400).json({
+                error: true,
+                message: 'User entity to create isn\'t valid',
+            });
+            break;
+
+        default:
+            res.status(500).json({
+                error: true,
+                message: err.message,
+            });
+    }
     next();
 }
 
