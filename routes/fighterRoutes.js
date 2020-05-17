1eabf0718cb095c035ecf6b443642c7e2cb3e82c
Viacheslav Glazhevskyi <glajik@gmail.com>
2020-05-17 09:31:38 +0300

    Implements GET /api/fighters - get all fighters

--- routes/fighterRoutes.js
+++ routes/fighterRoutes.js
@@ -6,5 +6,27 @@ const { createFighterValid, updateFighterValid } = require('../middlewares/fight
 const router = Router();
 
 // TODO: Implement route controllers for fighter
+router.use(createFighterValid);
+router.use(updateFighterValid);
+
+// Get all fighters
+router.get('/', (req, res, next) => {
+  try {
+    const result = FighterService.read();
+    if (!result.length) {
+      throw new Error('No any fighter');
+    }
+    res.data = result;
+  } catch (error) {
+    error.type = 'not_found';
+    res.err = error;
+  } finally {
+    next();
+  }
+  res.data = {};
+  next();
+});
+
+router.use(responseMiddleware);
 
 module.exports = router;
--- services/fighterService.js
+++ services/fighterService.js
@@ -2,6 +2,9 @@ const { FighterRepository } = require('../repositories/fighterRepository');
 
 class FighterService {
     // TODO: Implement methods to work with fighters
+    read() {
+        return FighterRepository.getAll();
+    }
 }
 
 module.exports = new FighterService();
